6145f1c47bb3a48fec891255dc341e92de94a2ed
Priya Chaudhary <95042955+PriyaDiswar@users.noreply.github.com>
2024-11-11 15:06:14 +0530

    Update Letter Combinations of a Phone Number.py

--- Backtracking/Letter Combinations of a Phone Number.py
+++ Backtracking/Letter Combinations of a Phone Number.py
@@ -38,3 +38,28 @@ class Solution:
         res=[]
         combi(0,[])
         return res
+
+
+
+
+                OR
+
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        d={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        
+
+        def combi(i,s):
+            if i==len(digits):
+                res.append(s)
+                return
+            for j in d[digits[i]]:
+                combi(i+1,s+j)
+    
+        res=[]
+        combi(0,"")
+        return res
